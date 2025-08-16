f4f78d5cbe644cfbd410294a13978da32b45c067
aleksandar-cvetkov <aleksandar.cvetkov@productdna.com>
2025-08-16 17:44:42 +0200

    Firebase configuration in progress

---
+++ src/app/core/firebase.config.ts
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+    apiKey: "AIzaSyAl20ZCCzIEZVYQGZxnl9_PNdNT3lyrNNs",
+    authDomain: "angular-firebase-app-ce2c0.firebaseapp.com",
+    projectId: "angular-firebase-app-ce2c0",
+    storageBucket: "angular-firebase-app-ce2c0.firebasestorage.app",
+    messagingSenderId: "864881525638",
+    appId: "1:864881525638:web:1bbaa72068334cdfa86207",
+    measurementId: "G-JYNHM7LR5T"
+  };
